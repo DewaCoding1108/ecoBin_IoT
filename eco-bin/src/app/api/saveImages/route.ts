98806ed67d5831378bc49f83ce91888adc22d974
I Dewa Made Manu P <18221047@std.stei.itb.ac.id>
2024-06-09 16:40:31 +0700

    fix api

--- eco-bin/src/app/api/saveImages/route.ts
+++ eco-bin/src/app/api/saveImages/route.ts
@@ -36,7 +36,7 @@ export async function POST(req:Request){
     // .catch(function(error:any) {
     //   console.log(error.message);
     // });
-    return response;
+    return new Response('ok');
   }
   catch(error){
     console.error('Error saving image:', error);
